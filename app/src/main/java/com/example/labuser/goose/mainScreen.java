5d993971b0a3390c983e5be405e5669bf8710ec6
LabUser <LabUser>
2016-11-03 13:39:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/labuser/goose/mainScreen.java
@@ -0,0 +1,13 @@
+package com.example.labuser.goose;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
